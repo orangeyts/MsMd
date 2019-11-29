5fdfeb960ba26090ed3584d20b155b66a24765a9
hs <112260085@qq.com>
2019-11-29 18:14:36 +0800

    优化导出

--- src/main/java/com/demo/vo/TbWeekReportVO.java
+++ src/main/java/com/demo/vo/TbWeekReportVO.java
@@ -2,6 +2,7 @@ package com.demo.vo;
 
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
 
@@ -15,13 +16,14 @@ public class TbWeekReportVO {
 
     @ExcelProperty("姓名")
     private String userName;
+    @ColumnWidth(80)
+    @ExcelProperty("工作描述")
+    private String content;
     @ColumnWidth(50)
     @ExcelProperty("总结")
     private String summary;
-    @ColumnWidth(50)
-    @ExcelProperty("工作描述")
-    private String content;
     @ExcelProperty("日期")
+    @DateTimeFormat("yyyy-MM-dd")
     @ColumnWidth(20)
     private Date reportDate;
 }
