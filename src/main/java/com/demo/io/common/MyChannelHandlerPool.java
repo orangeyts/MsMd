883d8b17d9d3a70b986d9e72635d0da6f8914df9
hs <112260085@qq.com>
2019-12-04 18:35:07 +0800

    群发消息和私信

--- src/main/java/com/demo/io/common/MyChannelHandlerPool.java
+++ src/main/java/com/demo/io/common/MyChannelHandlerPool.java
@@ -3,21 +3,76 @@ package com.demo.io.common;
 import io.netty.channel.ChannelId;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 /**
- * https://www.jianshu.com/p/75865e54b01f
+ * 连接池管理
+ * 参考 https://www.jianshu.com/p/75865e54b01f
+ * @author orange
+ * @date 2019/12/04
  */
+@Slf4j
 public class MyChannelHandlerPool {
 
     public MyChannelHandlerPool(){}
 
     public static Map<String, ChannelId> channelIdMap = new HashMap<>();
 
+    /**
+     * 群组管理
+     * 例如一个工程构建,会产生一个ChannelGroup,后续查看这个工程 滚动输出信息的客户端 都要加入到这个组里
+     * 这样 就可以广播消息到这群组了
+     *
+     * key : buildId
+     * value : uid List
+     * 表示这个组里里面加入了哪些 uid,消息分发的时候，到channelIdMap(总的链接列表)取到对应的Channel依次发送 群消息即可
+     */
+    public static Map<String, List<String>> channelGroupMap = new ConcurrentHashMap<>();
+
     public static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
+
+    /**
+     * 创建 或者 加入组
+     * @param groupId
+     * @param uid
+     */
+    public static void createOrJoinGroup(String groupId, String uid){
+        List<String> oldGroup = channelGroupMap.get(groupId);
+        if (oldGroup != null && oldGroup.size() > 0){
+            //已经存在这个组,把新的用户加入这个组
+            oldGroup.add(uid);
+        }else {
+            //创建一个新的组
+            List<String> newGroup = new ArrayList<>();
+            newGroup.add(uid);
+            channelGroupMap.put(groupId,newGroup);
+        }
+    }
+
+    /**
+     * 群发消息
+     * @param groupId
+     * @param message
+     */
+    public static void sendGroupMsg(String groupId, String message){
+        List<String> groupUsers = channelGroupMap.get(groupId);
+        if (groupUsers == null && groupUsers.size() == 0){
+            log.info("该群组 [{}] 没有人");
+            return;
+        }
+        for(String userId : groupUsers){
+            ChannelId channelId = channelIdMap.get(userId);
+            MyChannelHandlerPool.channelGroup.find(channelId).writeAndFlush(new TextWebSocketFrame(message));
+        }
+    }
 }
--- src/main/java/com/demo/io/common/MyWebSocketHandler.java
+++ src/main/java/com/demo/io/common/MyWebSocketHandler.java
@@ -8,23 +8,22 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 public class MyWebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("与客户端建立连接，通道开启！");
-
-        //添加到channelGroup通道组
         MyChannelHandlerPool.channelGroup.add(ctx.channel());
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("与客户端断开连接，通道关闭！");
-        //添加到channelGroup 通道组
         MyChannelHandlerPool.channelGroup.remove(ctx.channel());
     }
 
@@ -34,12 +33,43 @@ public class MyWebSocketHandler extends SimpleChannelInboundHandler<TextWebSocke
         String msgText = msg.text();
         JSONObject jsonObject = JSONObject.parseObject(msgText);
         Map<String, Object> map = jsonObject;
+        String groupId = jsonObject.getString("groupId");
+        String userId = jsonObject.getString("userId");
+        String message = jsonObject.getString("message");
+        Integer type = jsonObject.getInteger("type");
         String longId = ctx.channel().id().asLongText();
         if (map.containsKey("login")) {
             login(map, ctx.channel().id());
+        }else if(type != null && type.intValue() == 1){
+            if (log.isInfoEnabled()){
+                log.info("用户 [{}] ,加入群组 [{}]",userId,groupId);
+            }
+            joinGroup(groupId,userId);
+        }else if(type != null && type.intValue() == 2){
+            if (log.isInfoEnabled()){
+                log.info("群发消息 [{}]  [{}]",groupId,message);
+            }
+            sendGroupMsg(groupId,message);
         } else {
+            if (log.isInfoEnabled()){
+                log.info("私信消息 [{}]  ",map);
+            }
             sendToSomeone(map);
         }
+    }
+
+    //群发消息
+    private void sendGroupMsg(String groupId, String message) {
+        MyChannelHandlerPool.sendGroupMsg(groupId,message);
+    }
+
+    /**
+     * 加入一个组
+     * @param groupId
+     * @param userId
+     */
+    private void joinGroup(String groupId, String userId) {
+        MyChannelHandlerPool.createOrJoinGroup(groupId,userId);
     }
 
     //ping、pong
@@ -62,8 +92,8 @@ public class MyWebSocketHandler extends SimpleChannelInboundHandler<TextWebSocke
     }
     private void sendToSomeone(Map<String, Object> map) {
 //        ChannelId channelId = (ChannelId) redisOperator.getObject(6, map.get("toId").toString());
-        ChannelId fromId = MyChannelHandlerPool.channelIdMap.get(map.get("fromId"));
-        MyChannelHandlerPool.channelGroup.find(fromId).writeAndFlush(new TextWebSocketFrame(map.toString()));
+//        ChannelId fromId = MyChannelHandlerPool.channelIdMap.get(map.get("fromId"));
+//        MyChannelHandlerPool.channelGroup.find(fromId).writeAndFlush(new TextWebSocketFrame(map.toString()));
 
         ChannelId toId = MyChannelHandlerPool.channelIdMap.get(map.get("toId"));
         MyChannelHandlerPool.channelGroup.find(toId).writeAndFlush(new TextWebSocketFrame(map.toString()));
